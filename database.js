e791e22e8b385ffee7bc2bcd82663bddb5e4bdde
ophirstack-hash <ophirstack@gmail.com>
2026-09-25 11:01:58 +0100

    Add files via upload

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'trannity.db');
+const dbPath = process.env.DATABASE_PATH || path.resolve(__dirname, 'trannity.db');
 const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
